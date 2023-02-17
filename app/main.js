1f98d2e8c324805981c295745b7071578ec3a2dd
Johanna Wirell <jw223gx@student.lnu.se>
2023-02-17 13:19:58 +0100

    Add controls so screen can be moved

--- app/main.js
+++ app/main.js
@@ -1,5 +1,6 @@
 import './css/index.css'
 import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 import { torus } from './components/object'
 import { 
   pointLight,
@@ -17,37 +18,53 @@ const VIEW_FRUSTUM1 = 0.1
 const VIEW_FRUSTUM2 = 1000
 const CAMERA_POSITION_Z = 30
 
-const scene = new THREE.Scene()
-const camera = new THREE.PerspectiveCamera(
-  FIELD_OF_VIEW,
-  ASPECT_RATIO,
-  VIEW_FRUSTUM1,
-  VIEW_FRUSTUM2
-  )
-const renderer = new THREE.WebGL1Renderer({
-  canvas: document.querySelector('#background')
-})
+const main = () => {
+  try {
+    const scene = new THREE.Scene()
+    const camera = new THREE.PerspectiveCamera(
+      FIELD_OF_VIEW,
+      ASPECT_RATIO,
+      VIEW_FRUSTUM1,
+      VIEW_FRUSTUM2
+      )
+    const renderer = new THREE.WebGL1Renderer({
+      canvas: document.querySelector('#background')
+    })
 
-renderer.setPixelRatio(window.devicePixelRatio)
-renderer.setSize(window.innerWidth, window.innerHeight) // Set to full screen
-camera.position.setZ(CAMERA_POSITION_Z) // Move camera
+    renderer.setPixelRatio(window.devicePixelRatio)
+    renderer.setSize(window.innerWidth, window.innerHeight) // Set to full screen
+    camera.position.setZ(CAMERA_POSITION_Z) // Move camera
 
-scene.add(
-  torus,
-  pointLight,
-  ambientLight,
-  lightHelper,
-  gridHelper
-)
+    const controls = new OrbitControls(
+      camera,
+      renderer.domElement
+    )
 
-function animate (){
-  requestAnimationFrame(animate)
+    const animate = () => {
+      requestAnimationFrame(animate)
+    
+      torus.rotation.x += 0.001
+      torus.rotation.y += 0.005
+      torus.rotation.z += 0.01
+    
+      controls.update()
+    
+      renderer.render(scene, camera)
+    }
 
-  torus.rotation.x += 0.001
-  torus.rotation.y += 0.005
-  torus.rotation.z += 0.01
+    scene.add(
+      torus,
+      pointLight,
+      ambientLight,
+      lightHelper,
+      gridHelper
+    )
 
-  renderer.render(scene, camera)
+
+    animate()
+  } catch (err) {
+    console.log(err)
+  }
 }
 
-animate()
+main()
