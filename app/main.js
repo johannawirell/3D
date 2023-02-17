d8d2d18d94603544c35a4894bc6f1f39ad003992
Johanna Wirell <jw223gx@student.lnu.se>
2023-02-17 20:21:38 +0100

    Add controller attributes

--- app/main.js
+++ app/main.js
@@ -8,12 +8,15 @@ import {
   ambientLight
 } from './components/light'
 
-const FIELD_OF_VIEW = 75
+const FIELD_OF_VIEW = 50
 const ASPECT_RATIO = window.innerWidth / window.innerHeight
 const VIEW_FRUSTUM1 = 0.1
 const VIEW_FRUSTUM2 = 1000
 const CAMERA_POSITION_Z = 50
 const CAMERA_POSITION_Y = 10
+const MIN_DISTANSE = 5
+const MAX_DISTANCE = 150
+const POLAR_ANGLE = Math.PI / 2 - 0.05
 
 const main = () => {
   try {
@@ -50,15 +53,22 @@ const main = () => {
 
     // RENDERER
     const renderer = new THREE.WebGL1Renderer({
-      canvas: document.querySelector('#background')
+      canvas: document.querySelector('#background'),
+      // antialias: true BEHÖVS?
     })
     renderer.setPixelRatio(window.devicePixelRatio)
     renderer.setSize(window.innerWidth, window.innerHeight) // Set to full screen
 
+    // CONTROLS
     const controls = new OrbitControls(
       camera,
       renderer.domElement
     )
+    controls.enableDamping = true
+    controls.minDistance = MIN_DISTANSE
+    controls.maxDistance = MAX_DISTANCE 
+    controls.enablePan = true
+    controls.maxPolarAngle = POLAR_ANGLE
 
     animate()
   } catch (err) {
