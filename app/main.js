4c5ee8a7f8dfe4da45100b4771a2f127bd2edfd4
Johanna Wirell <jw223gx@student.lnu.se>
2023-02-17 12:49:31 +0100

    Create scene camera and renderer

--- app/main.js
+++ app/main.js
@@ -1,8 +1,25 @@
 import './css/index.css'
+import * as THREE from 'three'
 
-document.querySelector('#app').innerHTML = `
-  <div>
-   Hello world!
-  </div>
-`
+const FIELD_OF_VIEW = 75
+const ASPECT_RATIO = window.innerWidth / window.innerHeight
+const VIEW_FRUSTUM1 = 0.1
+const VIEW_FRUSTUM2 = 1000
+const CAMERA_POSITION_Z = 30
 
+const scene = new THREE.Scene()
+const camera = new THREE.PerspectiveCamera(
+  FIELD_OF_VIEW,
+  ASPECT_RATIO,
+  VIEW_FRUSTUM1,
+  VIEW_FRUSTUM2
+  )
+const renderer = new THREE.WebGL1Renderer({
+  canvas: document.querySelector('#background')
+})
+
+renderer.setPixelRatio(window.devicePixelRatio)
+renderer.setSize(window.innerWidth, window.innerHeight) // Set to full screen
+camera.position.setZ(CAMERA_POSITION_Z) // Move camera
+
+renderer.render(scene, camera)
