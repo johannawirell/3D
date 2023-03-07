d21be16af4bdd8aa3c77d65fa87622050ee57d09
Johanna Wirell <jw223gx@student.lnu.se>
2023-03-07 13:15:48 +0100

    Update plane

--- app/components/objects/plane.js
+++ app/components/objects/plane.js
@@ -3,14 +3,20 @@ import * as THREE from 'three'
 const IMAGE = '../../img/plane.jpg'
 
 const texture = new THREE.TextureLoader().load(IMAGE)
+texture.wrapS = THREE.RepeatWrapping
+texture.wrapT = THREE.RepeatWrapping 
+texture.repeat.set(40, 20)
 
 export const plane = new THREE.Mesh(
     new THREE.PlaneGeometry(
         window.innerWidth,
-        window.innerHeight
+        window.innerHeight,
+
+
     ),
     new THREE.MeshBasicMaterial( { map: texture })
 )
 
 plane.rotation.x = -Math.PI / 2 // Rotate 90°
 plane.position.set(0, 0, 0)
+// plane.scale.normalize().multiplyScalar(1)
