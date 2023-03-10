54c5242d7fe3d7ef63f8ac2f929b1c0eb62376ed
Johanna Wirell <jw223gx@student.lnu.se>
2023-03-10 10:28:50 +0100

    Update css

--- app/components/descriptions/gameDescription.js
+++ app/components/descriptions/gameDescription.js
@@ -3,5 +3,11 @@ import { Description } from './description'
 export class GameDescrition extends Description {
     constructor(params) {
         super(params)
+        this.#updateDiv()
+    }
+
+    #updateDiv() {
+        console.log(this.div)
+        this.div.classList.add('game-description')
     }
 }
