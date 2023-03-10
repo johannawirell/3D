12e38b66b8736f501fab1dd5414722e68f50cfef
Johanna Wirell <jw223gx@student.lnu.se>
2023-03-10 12:40:27 +0100

    Add content

--- app/components/descriptions/gameDescription.js
+++ app/components/descriptions/gameDescription.js
@@ -7,8 +7,7 @@ export class GameDescrition extends Description {
     }
 
     #updateDiv() {
-        this.div.classList.add('game-description')
-        this.div.innerText = 'hej'
+        this.div.classList.add('game-description-container')
     }
 
     updateWidthAndHeight (newWidth, newHeight) {
@@ -16,6 +15,22 @@ export class GameDescrition extends Description {
     }
 
     addContent() {
+        // Skapa en h1-element
+        const h1 = document.createElement('h1')
+        h1.textContent = 'Välkommen till Johanna Wirells CV spel'
+
+        // Skapa ett p-element
+        const p = document.createElement('p')
+        p.textContent = 'Flytta spelaren runt på skärmen för att se hur mitt liv ser ut.'
+
+        // Skapa en button-element
+        const button = document.createElement('button')
+        button.textContent = 'Starta'
+
+        // Lägg till elementen till dokumentet
+        this.div.appendChild(h1)
+        this.div.appendChild(p)
+        this.div.appendChild(button)
 
     }
 }
