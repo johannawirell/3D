bbca326dfb16a1152403c63e6f7f41b0474e1f1a
Johanna Wirell <jw223gx@student.lnu.se>
2023-03-10 13:24:16 +0100

    CSS description

--- app/components/descriptions/gameDescription.js
+++ app/components/descriptions/gameDescription.js
@@ -15,21 +15,50 @@ export class GameDescrition extends Description {
     }
 
     addContent() {
-        // Skapa en h1-element
         const h1 = document.createElement('h1')
         h1.textContent = 'Välkommen till Johanna Wirells CV spel'
 
-        // Skapa ett p-element
-        const p = document.createElement('p')
-        p.textContent = 'Flytta spelaren runt på skärmen för att se hur mitt liv ser ut.'
+        const descriptionDiv = document.createElement('div')
+        descriptionDiv.classList.add('description')
+        const p1 = document.createElement('p')
+        p1.textContent = 'Flytta spelaren runt på skärmen för att se hur mitt liv ser ut.'
+        descriptionDiv.appendChild(p1)
+        const p2 = document.createElement('p')
+        p2.textContent = 'Du flyttar spelaren enligt nedan beskrivning:'
+        descriptionDiv.appendChild(p2)
 
-        // Skapa en button-element
+        const keyDescription = document.createElement('div')
+        keyDescription.classList.add('description-keys')
+        const p3 = document.createElement('p')
+        p3.textContent = 'w - Framåt'
+        keyDescription.appendChild(p3)
+        const p4 = document.createElement('p')
+        p4.textContent = 's - Bakåt'
+        keyDescription.appendChild(p4)
+        const p5 = document.createElement('p')
+        p5.textContent = 'd - Höger'
+        keyDescription.appendChild(p5)
+        const p6 = document.createElement('p')
+        p6.textContent = 'a - Vänster'
+        keyDescription.appendChild(p6)
+        const p7 = document.createElement('p')
+        p7.textContent = 'shift - Spring'
+        keyDescription.appendChild(p7)
+        const p8 = document.createElement('p')
+        p8.textContent = 'space - hoppa'
+        keyDescription.appendChild(p8)
+
+        descriptionDiv.appendChild(keyDescription)
+
+        const p9 = document.createElement('p')
+        p9.textContent = '... och akta dig för monstret!'
+        descriptionDiv.appendChild(p9)
+
         const button = document.createElement('button')
         button.textContent = 'Starta'
 
-        // Lägg till elementen till dokumentet
         this.div.appendChild(h1)
-        this.div.appendChild(p)
+        this.div.appendChild(descriptionDiv)
         this.div.appendChild(button)
 
     }
