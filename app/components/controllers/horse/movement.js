176f9b83cc8388958e0c9575410a1005716273e1
Johanna Wirell <jw223gx@student.lnu.se>
2023-03-15 17:06:02 +0100

    Add vehicle

--- app/components/controllers/horse/movement.js
+++ app/components/controllers/horse/movement.js
@@ -1,13 +1,39 @@
 import * as THREE from 'three'
+import * as YUKA from 'yuka'
 
 export class Movement {
     velocity = new THREE.Vector3(0, 0, 2)
     currentPosition = new THREE.Vector3()
 
-    constructor(waypoints) {
-        this.waypoints = waypoints
-        this.speed = 0.5
-        this.waypointIndex = 0
+    constructor(target) {
+        this.target = target
+        
+        this.#createVehicle()
+    }
+
+    #createVehicle() {
+        this.vehicle = new YUKA.Vehicle()
+
+        this.vehicle.boundingRadius = 1.9
+        this.vehicle.smoother = new YUKA.Smoother(30)
+
+        this.#addPathToVehicle()
+    }
+
+    #addPathToVehicle() {
+        this.path = new YUKA.Path()
+        this.path.add( new YUKA.Vector3(-4, 0, -11))
+        this.path.add( new YUKA.Vector3(4, 0, -11))
+        this.path.add( new YUKA.Vector3(4, 0, 11))
+        this.path.add( new YUKA.Vector3(-4, 0, 11))
+
+        this.path.loop = true
+
+        this.vehicle.position.copy(this.path.current())
+
+        const followPathBehavior = new YUKA.FollowPathBehavior(this.path, 3)
+        this.vehicle.steering.add(followPathBehavior)
+        
     }
 
     #rotate(newPosition, target) {
