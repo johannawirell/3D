483a673812c322075798cf7081ba3db186ed9b7f
Johanna Wirell <jw223gx@student.lnu.se>
2023-03-16 14:02:39 +0100

    Update plane

--- app/components/objects/plane.js
+++ app/components/objects/plane.js
@@ -17,6 +17,7 @@ const SKYBOX = [
 
 export class Plane {
     numberOfTrees = 1
+    radius = 100
     constructor(scene, loadingManager, entityManager) {
         this.scene = scene
         this.loadingManager = loadingManager
@@ -43,27 +44,40 @@ export class Plane {
         texture.wrapS = THREE.MirroredRepeatWrapping;
         texture.wrapT = THREE.MirroredRepeatWrapping;
         
-        texture.repeat.set(50, 50)
+        texture.repeat.set(20, 20)
         return texture
     }
 
     #createPlane() {
         const texture = this.#createTextureSkybox()
         this.scene.background = texture
-        this.plane = new THREE.Mesh(
-            new THREE.PlaneGeometry(
-                window.innerWidth * 2,
-                window.innerHeight * 2,
-            ),
-            new THREE.MeshBasicMaterial( { map: this.#createTexturePlane() })
+
+        const planeGeometry = new THREE.PlaneGeometry(
+            window.innerWidth * 2,
+            window.innerHeight * 2,
+            50, 20, 50, 50// Add more segments to the geometry for smoother bending
         )
-        
+
+        // Bend the plane downwards
+        const vertices = planeGeometry.attributes.position.array
+        for (let i = 0; i < vertices.length; i += 3) {
+            const x = vertices[i]
+            const z = vertices[i + 2]
+            vertices[i + 2] = z - Math.sin(x / 10) * 20 // Adjust the amplitude and frequency of the wave as desired
+        }
+
+        const material = new THREE.MeshBasicMaterial({
+            map: this.#createTexturePlane(),
+        })
+
+        this.plane = new THREE.Mesh(planeGeometry, material)
+
         this.plane.castShadow = false
         this.plane.receiveShadow = true
         this.#position()
 
         // this.#addTrees()
-        this.scene.add(this.plane)    
+        this.scene.add(this.plane)   
     }
 
     #addTrees() {
