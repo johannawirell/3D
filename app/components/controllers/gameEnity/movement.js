086bc065c9c536f635bd6b38d324f6cf7437a756
Johanna Wirell <jw223gx@student.lnu.se>
2023-04-03 19:18:03 +0200

    Update horse

--- app/components/controllers/gameEnity/movement.js
+++ app/components/controllers/gameEnity/movement.js
@@ -36,34 +36,4 @@ export class Movement {
         this.vehicle.steering.add(followPathBehavior)
         
     }
-
-    #rotate(newPosition, target) {
-        // const forward = newPosition.clone().sub(target.position).normalize()
-        // const up =  new THREE.Vector3(0, 1, 0)
-        // const right = new THREE.Vector3().crossVectors(forward, up).normalize()
-
-        // const lookRotation = new THREE.Matrix4()
-        // lookRotation.set(
-        //   right.x, up.x, -forward.x, 0,
-        //   right.y, up.y, -forward.y, 0,
-        //   right.z, up.z, -forward.z, 0,
-        //   0, 0, 0, 1
-        // )
-      
-        // target.rotation.setFromRotationMatrix(lookRotation)
-      } 
-
-    move(time, target) {
-        // const nextWaypoint = this.waypoints[this.waypointIndex]
-        // const currentPosition = target.position
-        // const newPosition = currentPosition.clone().lerp(nextWaypoint, this.speed * time)
-
-        // this.#rotate(nextWaypoint.clone(), target)
-
-        // if (newPosition.distanceTo(nextWaypoint) < 10) {
-        //     this.waypointIndex = (this.waypointIndex + 1) % this.waypoints.length
-        // }
-
-        // return newPosition
-    }
 }
