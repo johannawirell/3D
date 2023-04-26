c85be618b90c4c9f124e2e66159c4cdd5653409f
Johanna Wirell <jw223gx@student.lnu.se>
2023-04-26 17:23:17 +0200

    Update shader

--- app/components/objects/shaders/shader.js
+++ app/components/objects/shaders/shader.js
@@ -5,16 +5,15 @@ export const glassShader = {
     },
     fragmentShader: `
         uniform float uTime;
-        void main() {
-            gl_FragColor = vec4(1.0, 1.0, 1.0, 1.0);
-            if (mod(gl_FragCoord.x + gl_FragCoord.y + uTime * 10.0, 20.0) < 10.0) {
-                gl_FragColor = vec4(0.5, 0.5, 0.5, 1.0);
-            }
-        }
+        
+        void main() { 
+            float alpha = 0.5 + 0.5 * sin(gl_FragCoord.x * 10.0 + uTime);
+            gl_FragColor = vec4(1.0, 1.0, 1.0, alpha); 
+        } 
     `,
     vertexShader: `
-        void main() {
-            gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
-        }
+    void main() { 
+        gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
+    } 
     `,
 }
